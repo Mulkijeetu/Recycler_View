bca0694a82a3416ce130ae1edec675ed7ca1bd2d
Jithesh K <jithesh@example.com>
2017-03-14 17:06:47 +0530

    first commit

---
+++ app/src/main/java/com/example/davlin_6/recycler_view/DataModel.java
@@ -0,0 +1,36 @@
+package com.example.davlin_6.recycler_view;
+
+/**
+ * Created by davlin-6 on 14/3/17.
+ */
+
+public class DataModel {
+
+    String name;
+    String version;
+    int id_;
+    int image;
+
+    public DataModel(String name, String version, int id_, int image) {
+        this.name = name;
+        this.version = version;
+        this.id_ = id_;
+        this.image=image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public int getId() {
+        return id_;
+    }
+}
